cab92f9fecc516899a10cf3f8acfb199930935ba
Leandro Matos <leandro.matos@live.com>
2023-05-23 14:51:48 -0300

    Operadores Booleanos

---
+++ operadores_booleanos.js
@@ -0,0 +1,42 @@
+// Operadores Booleanos
+
+// Igualdade:          == (ou ===)
+// Desigualdade:       != (ou !==)
+// Maior que:          >
+// Maior ou Igual:     >=
+// Menor que:          <
+// Menor ou Igual:     <=
+
+const numero = 10;
+
+console.log(numero == 10)
+console.log(numero > 20)
+
+console.log(numero == 10)
+console.log(10 == '10')
+console.log(10 === '10')
+
+console.clear()
+
+// Conjunções Lógicas
+
+
+// AND => &&
+
+let idade = 26;
+let tenhoCNH = true;
+
+const possoDirigir = idade >= 18 && tenhoCNH === true
+console.log('Posso dirigir?', possoDirigir)
+
+// OR  => ||
+
+idade = 40;
+
+const votoFacultativo = idade < 18 || idade >= 70
+
+// NOT => !
+
+const estouGostandoDoCurso = false;
+
+console.log(!estouGostandoDoCurso)
