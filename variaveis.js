3bf90f38f7c13033e40f0ddac8a943ac265d719a
Leandro Matos <leandro.matos@live.com>
2023-05-23 13:59:51 -0300

    Aula 02 - JS (Variáveis)

---
+++ variaveis.js
@@ -0,0 +1,31 @@
+// Variáveis
+
+let idade = 26; // Criando a primeira variável
+
+console.log(idade)
+
+// Tipos de Variáveis
+
+let numero = 30               // Number
+let altura = 1.63             // Number
+let nome = 'Leandro'          // String (texto)
+let estudando = true          // Booleano (true or false)
+
+console.log(numero, typeof numero)
+console.log(altura, typeof altura)
+console.log(nome, typeof nome)
+console.log(estudando, typeof estudando)
+
+let outra
+
+console.log(outra)
+
+let nula = null
+
+console.log(nula)
+
+var variavel = 30 // Não utilizar o var
+
+const constante = 100
+
+constante = 20
